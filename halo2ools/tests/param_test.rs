6d57cbe0e09f888a9257ebc65853c0879a0f1187
Paul Cheng <chengyuejia@foxmail.com>
2023-10-11 20:09:34 +0800

    opti

--- halo2ools/tests/param_test.rs
+++ halo2ools/tests/param_test.rs
@@ -2,6 +2,7 @@ use halo2_proofs::arithmetic::*;
 use halo2ools::params::*;
 
 // cargo test test_save_and_load  -- --nocapture
+// DEGREE=6 cargo test --test debug_msm test_save_and_load --features msm_cuda -- --nocapture
 #[test]
 fn test_save_and_load() {
     let param_10_file = "param_11.srs".to_string();
